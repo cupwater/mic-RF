653a9b9bc6d2ffd4280f9eda80ee16bd746db0a5
cupwater <baoyun20@gmail.com>
2016-07-31 15:40:32 +0800

    feature_id error

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # mic-RF
 I want to implement random forest algorithm on Intel Xeon Phi(MIC).
+
+算法流程：
+
+对样本集进行采样  ————>  构造决策树 （n棵）————>  形成森林
+
+构造决策树流程： 宽度优先。  
+
+重要的变量说明：
+
+
+int *samples_ids： 记录采样得到的样本集
+float *sqr_y_list:
+
+splits_info: 如何存放的？ 先存储一个节点的所有属性
--- testRF.cpp
+++ testRF.cpp
@@ -1,4 +1,4 @@
-/**************************************************************************
+/***************************************************************************
 Copyright:
 Author: Peng Baoyun
 Data:   2016-07-30
@@ -11,20 +11,11 @@ Description:Regression Random  Forest implementation on Intel Xeon Phi(MIC).
 #include <iostream>
 #include <math.h>
 #include <algorithm>
-#include <sys/time.h>
 #include <ctime>
-#include <errno.h>
-#include <cassert>
-#include <omp.h>
-#include <getopt.h>
 #include <stdio.h>  
-#include <unistd.h>
 
 using namespace std;
 
-#define LOG_ERROR_(message) fprintf(stderr, "%s:%d:%s():  %s", __FILE__, __LINE__, __FUNCTION__, message); 
-#define LOG_WARNING_(message) fprintf(stderr, "%s:%d:%s (): %s", __FILE__, __LINE__, __FUNCTION__, message); 
-#define LOG_NOTICE_(message) fprintf(stderr , "%s",  message);
 #define sign(val) val>EPSI?1:(val<(-EPSI)?-1:0)
 #define square(x) (x*x)
 
@@ -32,9 +23,9 @@ using namespace std;
 #define MAX_CHILDS_NUM 	128
 #define MAX_NODES_NUM  	512
 #define MAX_TREE_NUM 	200
-#define MAX_ERROR		1e10
+#define MAX_ERROR		1000000000
 
-#pragma offload_attribute(push,target(mic))
+//#pragma offload_attribute(push,target(mic))
 
 /* configuration information for constructing forest */
 typedef struct {   
@@ -119,7 +110,6 @@ Input:
 	positions: int[]: the samples positions array recording the index of nodes
 	sqr_y_list: float[]: the samples square value in feas array
 	q: QNode[] : the nodes needing to split in the same depth 
-	samples_ids: int[] : the index of samples in dataVec 
 	splits_info: SplitInfo : the temporary split information 
 							for sampled features of all nodes in the same depth
 	treeNodes: TNode*  :  the root node of current tree 
@@ -130,19 +120,28 @@ Output:
 Return:         NULL
 *************************************************/ 
 inline void find_splits(FPpair *fppairs, int samples_num, int feature_id, int *positions, 
-		float *sqr_y_list, QNode *q, int *samples_ids, SplitInfo *splits_info,
-		 TNode *treeNodes, int sampled_fea_num)
+		float *sqr_y_list, QNode *q, SplitInfo *splits_info,
+		 TNode *treeNodes, int sampled_fea_num, int curr_childs_num)
 {
 	/* the samples sum value in left child nodes if splitting */
-	float *left_childs_sum = (float*) malloc(sizeof(float) * MAX_CHILDS_NUM);
+	float *left_childs_sum = (float*) malloc(sizeof(float) * curr_childs_num);
 	/* the samples number in left child nodes if splitting */	
-	int	  *left_childs_num = (int *) malloc(sizeof(int) * MAX_CHILDS_NUM);	
+	int	  *left_childs_num = (int *) malloc(sizeof(int) * curr_childs_num);	
+    /* the samples square sum value in left child nodes if splitting */
+    float *left_childs_sqrsum = (float*) malloc(sizeof(float) * curr_childs_num);
+
+	for(int i=0; i<curr_childs_num; i++)
+	{
+		left_childs_sum[i] = 0.0f;
+		left_childs_num[i] = 0.0f;
+		left_childs_sqrsum[i] = 0.0f;
+	}
 
 	/* traversal all values in feas to find the best split pos */
 	for(int i=0; i<samples_num; i++)
 	{
 		/* the index in dataVec of current sample  */
-		int sid = samples_ids[fppairs[i].pos];
+		int sid = fppairs[i].pos;
 		/* the index of node in current depth containing current sample */
 		int cid = positions[fppairs[i].pos];
 		/* the index of node's father node */
@@ -150,9 +149,15 @@ inline void find_splits(FPpair *fppairs, int samples_num, int feature_id, int *p
 		
 		left_childs_sum[cid] += yVec[sid];
 		left_childs_num[cid]++;
+
 		/* computing the split gain of current split pos, split_gain = sum^2/N - sum1^2/L - sum2^2/R */
-		float tempGain = q[cid].err - square(left_childs_sum[cid]) / left_childs_num[cid] + 
-			square(treeNodes[pid].sum_y - left_childs_sum[cid]) / (q[cid].cnt - left_childs_num[cid]);
+        float &ss0 = left_childs_sum[cid];
+        float ss1 = treeNodes[pid].sum_y - ss0;
+		
+		float tempGain = treeNodes[pid].sum_sqr_y - (ss0*ss0 / left_childs_num[cid]) - 
+			ss1*ss1 / (q[cid].cnt - left_childs_num[cid]);
+
+        printf("temp Gain is : %f \n", tempGain);
 		
 		SplitInfo &bsplit_info = splits_info[cid * sampled_fea_num +feature_id];
 		
@@ -163,10 +168,10 @@ inline void find_splits(FPpair *fppairs, int samples_num, int feature_id, int *p
 				bsplit_info.bsplit = fppairs[feature_id*samples_num + i].val;
 				bsplit_info.cnt[0] = left_childs_num[cid];	
 				bsplit_info.cnt[1] = q[cid].cnt - left_childs_num[cid];
-				bsplit_info.sum_y[0] = left_childs_sum[cid];
-				bsplit_info.sum_y[1] = treeNodes[pid].sum_y - left_childs_sum[cid];
-				bsplit_info.sum_sqr_y[0] = left_childs_sum[cid];
-				bsplit_info.sum_sqr_y[1] = treeNodes[pid].sum_y - left_childs_sum[cid];
+				bsplit_info.sum_y[0] = ss0;
+				bsplit_info.sum_y[1] = ss1;
+				bsplit_info.sum_sqr_y[0] += square(yVec[sid]);
+				bsplit_info.sum_sqr_y[1] = treeNodes[pid].sum_sqr_y - bsplit_info.sum_sqr_y[0];
 		}
 	}
 
@@ -186,7 +191,6 @@ Input:
 	int *positions: the positions(index of tree node) of samples in current depth
 	int &currLevel_childs_num: the number of nodes in current depth
 	int samples_num:  the number of samples
-	config: configuration of the forest
 Output:
 	new_q :  get the nodes needing to split in next depth 
 	positions:  the positions(index of tree node) of samples 
@@ -224,7 +228,6 @@ void update_queue(TNode *tree, int &nodes_num, SplitInfo *best_split_info, QNode
 	}
 	currLevel_childs_num = childs_num;
 
-	 #pragma omp parallel for schedule(static)
 	for (int i = 0; i < samples_num; i++) {
 		int &pos = positions[i];
 		if (pos >= 0 && best_split_info[pos].bind >=  0) {
@@ -265,13 +268,13 @@ void buildDecisionTree(int *samples_ids, int samples_num, ForestConfig &config,
 	int   *positions  = (int  *) malloc(sizeof(int) * MAX_CHILDS_NUM);		
    
     int   currLevel_childs_num = 0;	/* the nodes number in the sample tree's depth */
-	int   sampled_fea_num = sqrt(config.max_feature);/* features number when splitting at tree's node */
+	int   sampled_fea_num = sqrt((float)config.max_feature);		/* features number when splitting at tree's node */
 	
 	/* the nodes needing to splitting in the sample depth */
 	QNode *q = (QNode *) malloc(sizeof(QNode) *  MAX_CHILDS_NUM);
 	
 	/* fppairs :storing the samples data by column(features) instead of by row(samples) */
-	FPpair *fppairs = (FPpair*) malloc( sizeof(FPpair) * config.data_num * config.max_feature);
+	FPpair *fppairs = (FPpair*) malloc( sizeof(FPpair) * samples_num * config.max_feature);
 	
 	/* splits_info: storing the splitting informations for all features and all nodes */
 	SplitInfo *splits_info = (SplitInfo *) malloc(sizeof(SplitInfo) * MAX_CHILDS_NUM * sampled_fea_num);
@@ -288,16 +291,16 @@ void buildDecisionTree(int *samples_ids, int samples_num, ForestConfig &config,
 		sum_y += yVec[sid];
 		sum_sqr_y += sqr_y_list[i];                
 		positions[i] = 0;
-		for(int j = 0; j < config.max_feature; j++, fid++)
+		for(int j = 0; j < config.max_feature; j++)
 		{
-			fppairs[j * config.data_num + i].pos = i;
-			fppairs[j * config.data_num + i].val = dataVec[ fid ];
+			fppairs[j * samples_num + i].pos = sid;
+			fppairs[j * samples_num + i].val = dataVec[ fid + j ];
 		}
 	}
 
 	/* sorting the features value by ascending */
 	for (int i = 0; i < config.max_feature; i++)
-	    sort((fppairs + i*config.data_num), (fppairs + (i+1) * config.data_num), mySmaller);           
+	    sort((fppairs + i*samples_num), (fppairs + (i+1) * samples_num), mySmaller);           
     
 	/* adding the root node in this tree */
 	nodes_num = 0;
@@ -324,8 +327,9 @@ void buildDecisionTree(int *samples_ids, int samples_num, ForestConfig &config,
 			best_split_info[i].err =  MAX_ERROR;  
 			best_split_info[i].bind =  -1;             
 			for (int j = 0; j <  sampled_fea_num; j++) {                    
-				splits_info[i*sampled_fea_num + j].err = q[j].err;
-				splits_info[i*sampled_fea_num + j].bind = -1;
+				splits_info[i*currLevel_childs_num + j].err = MAX_ERROR;
+				splits_info[i*currLevel_childs_num + j].bind = -1;
+				splits_info[i*currLevel_childs_num + j].sum_sqr_y[0] = 0;
 			}
 		}
 
@@ -335,7 +339,7 @@ void buildDecisionTree(int *samples_ids, int samples_num, ForestConfig &config,
 			/* randomly select feature to find split position */
 			int feature_id = (int) rand() % config.max_feature;
 			find_splits( &(fppairs[feature_id * samples_num]), samples_num, feature_id, positions, 
-					sqr_y_list, q, samples_ids, splits_info, treeNodes, sampled_fea_num);
+					sqr_y_list, q, splits_info, treeNodes, sampled_fea_num, currLevel_childs_num);
 		}
 
 		/* merge split positions on all features to a best splitting */
@@ -362,6 +366,7 @@ void buildDecisionTree(int *samples_ids, int samples_num, ForestConfig &config,
 	free(q);
 }
 
+
 /************************************************* 
 Description:   building forest
 Input:        
@@ -385,278 +390,48 @@ void buildForest(float *dataVec, float *yVec, ForestConfig &config)
 	}
 }
 
-#pragma offload_attribute(pop)
 
-/************************************************* 
-Description:   get the maximum features number for a sample(string format)
-Calls:          read_conf_file()
-Input:          line:String, the sample
-Return:         the maximum features number 
-*************************************************/ 
-int max_feature_label(std::string line)
+/********************************************************************
+test function, get the test samples
+*********************************************************************/
+void testData(ForestConfig &config)
 {
-	int start = 0;
-	int fid;
-	int max_fid = 0;
-	int len = line.length();
+	config.data_num = 10;	 /* samples number. */
+	config.max_feature = 3; /* sample's features number */
+	config.tree_num = 2;	 /* trees number in forest */
+	config.depth = 3;		 /* maximum depth in trees */
+	config.min_children = 1;/* minimum samples number in leaf nodes */
+	config.bootstrap = -0.1; /* sampling ratio for samples */
+	config.nthread = 1;	 /* maximum parallel threads number */
 
-	for(int i = 0; i < len; i++) {
-		if(line[i] == ' ') {
-			start = i+1;
-		}
-		else if(line[i] == ':') {
-			if(sscanf(line.substr(start, i  - start).c_str(), "%d", &fid) == 1) {
-				if(max_fid < fid) {
-					max_fid = fid;
-				}
-			}	
-		}
-	} 
-	return max_fid;
-}
+	dataVec = (float*) malloc(sizeof(float ) * config.data_num * config.max_feature);
+	yVec = (float *) malloc(sizeof( float) * config.data_num);
 
-/************************************************* 
-Description:   splitting the string line by separator
-Calls:          read_conf_file()
-Input:          line:String, the sample
-Output:			the string array after splitting
-Return:         the elements number in items 
-*************************************************/ 
-int splitline(std::string line, std::string items[], int  items_num, const char separator)
-{
-	if(items == NULL || items_num <= 0) {
-		return -1;
-	}
-	int n = line.length();
-	int j = 0;
-	int start = 0;
-
-	for(int i = 0; i < n; i++) {
-		if(line[i] == separator) {
-			if(j < items_num && start < n) {
-				items[j++] = line.substr (start, i-start);
-				start = ++i;
-			}
-		}
-	}
-	if(j < items_num && start < n) {
-		items[j++] = line.substr(start, n-start);
-	}
-	return j;
-}
-
-bool has_colon(std::string item)
-{
-	for(int i = 0; i < (int)item.size(); i++)
+	/* fill the test data */
+	for(int i=0; i<config.data_num; i++)
 	{
-		if(item[i] == ':')
-			return true;
-	}
-	return false;
-}
-
-/************************************************* 
-Description:   print help information
-*************************************************/ 
-int print_usage (FILE* stream, char* program_name)
-{
-	fprintf (stream, "Usage: %s options [ input file  ... ]\n", program_name);
-	fprintf (stream, " -h --help.\n"
-		" -r --sample_feature_ratio Feature ratio  learning rate.\n"
-		" -t --tree_num GBDT #trees in the model \n"
-		" -s --shrink : learning rate\n"
-		" -n --min_node_size : stop tree growing,  minimum samples in leaf node.\n"
-		" -d --max_depth stop tree growing,  maximum depth of the tree.\n"
-		" -m --model_out .\n"
-		" -f --train_file .\n"
-		);
-
-	return 0;
-}
-
-
-/************************************************* 
-Description:   getting the samples data and basic information from file
-*************************************************/ 
-int read_conf_file(ForestConfig& config, int argc, char*  argv[])
-{
-	if(argv == NULL)	{
-		LOG_ERROR_("Parameter error.");
-		return -1;
-	}
-
-	int ch;
-	double random_feature_ratio = -1;
-	char message[BUFFER_LENGTH];
-
-	const char* short_options = "h:r:t:n:d:m:f:q:";
-	char filename[BUFFER_LENGTH];
-	const struct option long_options[]={
-		{"help", 0, NULL, 'h'},
-		{"sample_feature_ratio", 1, NULL, 'r'},
-		{"tree_num", 1, NULL, 't'},
-		{"thread_num", 1, NULL, 'q'},
-		{"min_node_size", 1, NULL, 'n'},
-		{"max_depth", 1, NULL, 'd'},
-		{"model_out", 1, NULL, 'm'},
-		{"train_file", 1, NULL, 'f'},
-		{NULL, 0, NULL, 0}
-	};
-
-	while((ch = getopt_long (argc, argv,  short_options, long_options, NULL)) != -1)
-	{
-		switch(ch)
+		yVec[i] = i + 1.0f;
+		for(int j=0; j<config.max_feature; j++)
 		{
-		case 'h':
-			if(argc == 2)
-			{
-				print_usage(stderr, argv [0]);
-				return 1;
-			}	
-			else
-				return -1;
-		case 'q':
-			if(sscanf(optarg, "%d",  &config.nthread) != 1)
-			{
-				if(config.nthread < 1)
-				{
-					LOG_ERROR_("Get  thread number error.");
-					return -1;
-				}
-				
-			}
-			printf("nthread %d\n", config.nthread);
-			break;
-		case 'r':
-			if(sscanf(optarg, "%f",  &config.bootstrap) != 1)
-			{
-				printf("error 2\n");
-				LOG_ERROR_("Get  random_feature_ratio config error.");
-				return -1;
-			}
-			printf("bootstrap %f\n", config.bootstrap);
-			break;
-		case 't':
-			if(sscanf(optarg, "%d",  &config.tree_num) != 1)
-			{
-				LOG_ERROR_("Get tree_num  config error.");
-				return -1;
-			}
-			printf("tree number %d\n", config.tree_num);
-			break;
-		case 'n':
-			if(sscanf(optarg, "%d",  &config.min_children) != 1)
-			{
-				LOG_ERROR_("Get  min_node_size config error.");
-				return -1;
-			}
-			printf("min children  %d\n", config.min_children);
-			break;
-		case 'd':
-			if(sscanf(optarg, "%d",  &config.depth) != 1)
-			{
-				LOG_ERROR_("Get max_depth  config error.");
-				return -1;
-			}
-			printf("max depth%d\n", config.depth);
-			break;
-		case 'f':
-			if(strlen(optarg) <=  BUFFER_LENGTH)
-				strncpy((char*) (filename), optarg, BUFFER_LENGTH);
-			else
-			{
-				LOG_ERROR_("Get  train_filename config error.");
-				return -1;
-			}
-			break;
-		case 'm' :
-			//if(strlen(optarg) <=  BUFFER_LENGTH)
-			//strncpy((char*) (filename), optarg, BUFFER_LENGTH);
-			break;
-		case '?':
-			printf("error  1\n");
-			print_usage(stderr, argv[0]);
-			return -1;
-		default:
-			printf("error  3\n");
-			print_usage(stderr, argv[0]);
-			return -1;
+			dataVec[i*config.max_feature + j] = i + 1.0f;
 		}
-	}
-	ifstream fin(filename);
-	if(fin)
-	{
-		string line;
-		config.data_num = 0;
-		config.max_feature = 0;
-		int temp;
-		while(getline(fin, line))
-		{
-			temp = max_feature_label(line);
-			if(config.max_feature < temp)
-				config.max_feature =  temp;
-			config.data_num++;
-		}
-		fin.close();
-	}else {
-		printf("read file error!\n");
-		return -1;
 	}
 
-	printf("data number %d and max feature %d number!\n", config.data_num, config.max_feature);
-
-	//dataVec.resize (config.data_num*config.max_feature);
-	dataVec = (float*)malloc(sizeof(float) *  config.max_feature * config.data_num);
-	//yVec.resize(config.data_num);
-	yVec = (float*)malloc(sizeof(float) *  config.data_num);
-	string* items = new string[config.max_feature+1]; 
-	string line;
-	int x_read, count,cnt = 0, fid;
-	double value;
-	printf("start reading data \n");
-	ifstream fin1(filename);
-	while (getline(fin1, line) != NULL) {
-		count = splitline(line, items,  config.max_feature+5, ' '); 
-		sscanf(items[0].c_str(),"%1f",&value);
-		//printf("value %f \n", value);
-		//the first column is y
-		yVec[cnt] = value; 
-		int temp = cnt * config.max_feature;
-		for(int i = 1; i < count; i++) {
-			if(has_colon(items[i]))	{
-				//featureid1:value1  featureid2:value2 ... density matrix x
-				x_read = sscanf(items [i].c_str(),"%d:%lf", &fid, &value); 
-				dataVec[temp+fid] =  value;
-			}
-		}
-		cnt++;
-		if (cnt >= config.data_num)	{ 
-			break;
-		}
-	}
-	
-	fin1.close();
-	printf("reading data over!\n");
-	
-	delete[] items;
-	return 0;
 }
 
 int main(int argc, char* argv[])
 {
 	ForestConfig config;
-	read_conf_file(config, argc, argv);
+	//read_conf_file(config, argc, argv);
+
+	testData(config);
 	
 	double start_time, end_time;
 	
-	#pragma offload target(mic : 0)\
-    in(dataVec:length(config.data_num*config.max_feature) alloc_if(1))\
-    in(yVec:length(config.data_num) alloc_if(1))
-	//out(forestNodes:length(config.tree_num* MAX_NODES_NUM))\
-	//out(treeNodesNum:length(config.tree_num))
-	{
-		buildForest(dataVec, yVec, config);
-	}
+	buildForest(dataVec, yVec, config);
+
+	free(dataVec);
+	free(yVec);
 
     printf("forest building feature parallel executing time: %f\n", end_time - start_time);
 	
